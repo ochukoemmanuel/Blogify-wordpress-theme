4cace84a6a01f0291c406c264b4bd3d209423073
ochukoemmanuel <ochukoemmanuel07@gmail.com>
2021-09-25 01:55:56 +0100

    Updated images, make logo dynamic, make pages dynamic, make header menu and footer manu load from wordpress,

--- functions.php
+++ functions.php
@@ -1,5 +1,26 @@
 <?php
 
+// theme support
+function blogify_theme_support(){
+    add_theme_support( 'title-tag');
+    add_theme_support('custom-logo');
+}
+
+add_action('after_setup_theme', 'blogify_theme_support');
+
+// Menu 
+function blogify_menus(){
+    $locations = array(
+        'primary' => "Main menu",
+        'footer' => "Footer menu items"
+    );
+
+    register_nav_menus($locations);
+}
+
+add_action('init', 'blogify_menus');
+
+// Style and js
 function blogify_resgister_style(){
     $version = wp_get_theme()->get('Version');
     wp_enqueue_style('blogify-style', get_template_directory_uri() . "/style.css", array(), $version, 'all');
---
+++ header.php
@@ -0,0 +1,107 @@
+<!DOCTYPE html>
+<html lang="en" class="no-js" >
+<head>
+
+    <!--- basic page needs
+    ================================================== -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <script>
+        document.documentElement.classList.remove('no-js');
+        document.documentElement.classList.add('js');
+    </script>
+
+    <!-- favicons
+    ================================================== -->
+    <link rel="apple-touch-icon" sizes="180x180" href="<?php echo esc_url(site_url('/apple-touch-icon.png')); ?>">
+    <link rel="icon" type="image/png" sizes="32x32" href="<?php echo esc_url(site_url('/favicon-32x32.png')); ?>">
+    <link rel="icon" type="image/png" sizes="16x16" href="<?php echo esc_url(site_url('/favicon-16x16.png')); ?>">
+    <link rel="manifest" href="site.webmanifest">
+
+    <?php
+        wp_head();
+        ?>
+
+</head>
+
+
+<body id="top">
+
+
+    <!-- preloader
+    ================================================== -->
+    <div id="preloader">
+        <div id="loader" class="dots-fade">
+            <div></div>
+            <div></div>
+            <div></div>
+        </div>
+    </div>
+
+
+    <!-- page wrap
+    ================================================== -->
+    <div id="page" class="s-pagewrap ss-home">
+
+
+        <!-- # site header 
+        ================================================== -->
+        <header id="masthead" class="s-header">
+
+            <div class="s-header__branding">
+                <p class="site-title">
+                    <a href="<?php echo site_url() ?>" rel="home"><?php echo get_bloginfo('name'); ?></a>
+                </p>
+            </div>
+
+            <div class="row s-header__navigation">
+
+                <nav class="s-header__nav-wrap">
+    
+                    <h3 class="s-header__nav-heading">Navigate to</h3>
+
+                    <?php 
+                        wp_nav_menu(
+                            array('menu' => 'primary',
+                                'container' => '',
+                                'theme_location' => 'primary',
+                                'items_wrap' => '<ul id="" class="s-header__nav">%3$s</ul>'
+                            )
+                        );
+                        ?><!-- end s-header__nav -->
+
+                </nav> <!-- end s-header__nav-wrap -->
+    
+            </div> <!-- end s-header__navigation -->
+
+            <div class="s-header__search">
+
+                <div class="s-header__search-inner">
+                    <div class="row">
+    
+                        <form role="search" method="get" class="s-header__search-form" action="#">
+                            <label>
+                                <span class="u-screen-reader-text">Search for:</span>
+                                <input type="search" class="s-header__search-field" placeholder="Search for..." value="" name="s" title="Search for:" autocomplete="off">
+                            </label>
+                            <input type="submit" class="s-header__search-submit" value="Search"> 
+                        </form>
+    
+                        <a href="#0" title="Close Search" class="s-header__search-close">Close</a>
+    
+                    </div> <!-- end row -->
+                </div> <!-- s-header__search-inner -->
+    
+            </div> <!-- end s-header__search -->
+
+            <a class="s-header__menu-toggle" href="#0"><span>Menu</span></a>
+            <a class="s-header__search-trigger" href="#">
+                <svg width="24" height="24" fill="none" viewBox="0 0 24 24">
+                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M19.25 19.25L15.5 15.5M4.75 11C4.75 7.54822 7.54822 4.75 11 4.75C14.4518 4.75 17.25 7.54822 17.25 11C17.25 14.4518 14.4518 17.25 11 17.25C7.54822 17.25 4.75 14.4518 4.75 11Z"></path>
+                </svg>
+            </a>
+
+        </header> <!-- end s-header -->
+
+        
